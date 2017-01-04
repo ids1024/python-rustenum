b071b568a948fcbefcc3b2bcd8d6e9efdd2b2794
Ian Douglas Scott <ian@iandouglasscott.com>
2017-01-03 16:08:34 -0800

    Make Variants not an instance of the Enum

--- rustenum.py
+++ rustenum.py
@@ -1,24 +1,71 @@
 import types
 
 
-class RustEnumVariantBase(tuple):
-    def __new__(cls, *args):
-        if len(args) != cls._num:
-            name = type(cls).__name__ + '.' + cls.__name__
+class RustEnumVariant:
+    def __init__(self, enum_class, name, num):
+        self._enum_class = enum_class
+        self._name = name
+        self._num = num
+
+    def __call__(self, *args):
+        if len(args) != self._num:
+            name = self._enum_class.__name__ + '.' + self._name
             raise TypeError("Wrong number of arguments to " + name)
-        return super().__new__(cls, args)
+        return self._enum_class(self._name, args)
 
     def __repr__(self):
-        name = type(type(self)).__name__ + '.' + type(self).__name__
+        return self._enum_class.__name__ + '.' + self._name
+
+
+class RustEnumMeta(type):
+    def __new__(metacls, cls, bases, classdict):
+        variants = {}
+        impls = {}
+
+        for k in list(classdict):
+            if not k.startswith("_") and not k == "match":
+                if callable(classdict[k]):
+                    impls[k] = (classdict[k])
+                else:
+                    variants[k] = classdict[k]
+                del classdict[k]
+
+        classdict["_variants"] = variants;
+        classdict["_impls"] = impls;
+
+        instance = super().__new__(metacls, cls, bases, classdict)
+
+        for k, v in variants.items():
+            variant = RustEnumVariant(instance, k, v)
+            setattr(instance, k, variant)
+
+        return instance
+
+    def __repr__(self):
+        return "<rustenum '" + self.__name__ + "'>"
+
+    def impl(self, function):
+        self._impls[function.__name__] = function
+        return function
+
+
+class RustEnum(tuple, metaclass=RustEnumMeta):
+    def __new__(cls, variant, values):
+        instance = super().__new__(cls, values)
+        instance._variant = variant
+        return instance
+
+    def __repr__(self):
+        name = type(self).__name__ + '.' + self._variant
         return "{}({})".format(name, ', '.join(repr(i) for i in self))
 
     def __getattr__(self, name):
-        if name in type(type(self))._impls:
-            return types.MethodType(type(type(self))._impls[name], self)
+        if name in type(self)._impls:
+            return types.MethodType(type(self)._impls[name], self)
         raise AttributeError
 
     def match(self, **kwargs):
-        variants = [i.__name__ for i in type(type(self))]
+        variants = type(self)._variants
 
         for k in kwargs:
             if k not in variants and k != '_':
@@ -34,49 +81,7 @@ class RustEnumVariantBase(tuple):
             raise ValueError("Match not cover all cases.\n"
                              "Not covered: " + not_handled)
 
-        if type(self).__name__ in kwargs:
-            return kwargs[type(self).__name__](*self)
+        if self._variant in kwargs:
+            return kwargs[self._variant](*self)
         else:
             return kwargs['_']()
-
-
-class RustEnumMeta(type):
-    def __new__(metacls, cls, bases, classdict):
-        variants = {}
-        impls = {}
-
-        for k in list(classdict):
-            if not k.startswith("_"):
-                if callable(classdict[k]):
-                    impls[k] = (classdict[k])
-                else:
-                    variants[k] = classdict[k]
-                del classdict[k]
-
-        classdict["_variants"] = [];
-        classdict["_impls"] = impls;
-
-        instance = super().__new__(metacls, cls, bases, classdict)
-
-        for k, v in variants.items():
-            subinstance = instance(k, (RustEnumVariantBase,), {"_num": v})
-            instance._variants.append(subinstance)
-            setattr(instance, k, subinstance)
-
-        return instance
-
-    def __repr__(self):
-        return "<rustenum '" + self.__name__ + "'>"
-
-    def __iter__(self):
-        return iter(self._variants)
-
-    def impl(self, function):
-        self._impls[function.__name__] = function
-        return function
-
-
-class RustEnum(type, metaclass=RustEnumMeta):
-    def __repr__(self):
-        name = type(self).__name__ + '.' + self.__name__
-        return "<{}>".format(name)
